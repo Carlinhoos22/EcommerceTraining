c1326a13aa685ead7bd72bc97945e655ae803973
KayyLanne <kaylanne.regina@hotmail.com>
2023-11-07 19:48:10 -0300

    atualizando detailview e categorias

--- produtos/views.py
+++ produtos/views.py
@@ -1,12 +1,18 @@
+from typing import Any
 from django.shortcuts import render
 from produtos.models import Produto
 from django.views.generic import ListView, DetailView
 
 
 class ProdutoListView(ListView):
-    model = Produto
+    queryset = Produto.objects.filter(categoria__nome="livro")
     context_object_name = 'produtos'
     template_name = 'produtos/index.html'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['relogios'] = Produto.objects.filter(categoria__nome="Relogio")
+        return context
 
 
 class ProdutoDetailView(DetailView):
@@ -14,6 +20,12 @@ class ProdutoDetailView(DetailView):
     context_object_name = 'produto'
     template_name = 'produtos/detalhar_produto.html'
     pk_url_kwarg = 'id'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        produto = self.get_object()
+        context['relogios'] = Produto.objects.filter(categoria__nome=produto.categoria).exclude(pk=produto.pk)
+        return context
 
 
 #def produtos(request):
